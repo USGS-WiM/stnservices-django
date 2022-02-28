9ba08f05ebef123379360b365e4018c9c217d415
lprivette <lprivette@usgs.gov>
2022-02-28 18:32:04 -0500

    #8 - suggest change fixes

--- stnservices/stnapi/models.py
+++ stnservices/stnapi/models.py
@@ -3,11 +3,10 @@ from django.db import models
 
 class Agency(models.Model):
     """
-    Agencies
+    Agency
     """
 
-    agency_id = models.IntegerField(null=True, help_text='An integer value of the agency id for this agency')
-    agency_name = models.CharField(max_length=128, blank=True, default='', help_text='An alphanumeric value of the name of this agency')
+    name = models.CharField(max_length=128, blank=True, default='', help_text='An alphanumeric value of the name of this agency')
     address = models.CharField(max_length=150, ull=True, help_text='An alphanumeric value of the address for this agency')
     city = models.CharField(max_length=100, help_text='An alphanumeric value of the city for this agency')
     state = models.CharField(max_length=2, help_text='An alphanumeric value of the state for this agency')
@@ -18,25 +17,22 @@ class Agency(models.Model):
         db_table = "stn_agency"
         verbose_name_plural = "agencies"
         
-class Agencies(models.Model):
+class Approval(models.Model):
     """
-    Appoval
+    Approval
     """
 
-    approval_id = models.IntegerField(null=True, help_text='An integer value of the approval id for this approval')
     member_id = models.IntegerField(null=True, help_text='An integer value of the member id for this approval')
     approval_date = models.DateField(null=True, help_text='The date this object was last modified on in "YYYY-MM-DD" format')
 
     class Meta:
         db_table = "stn_approval"
-        verbose_name_plural = "approvals"
         
 class Contact(models.Model):
     """
     Contact
     """
 
-    contact_id = models.IntegerField(null=True, help_text='An integer value of the contact id for this contact')
     fname = models.CharField(max_length=100, blank=True, default='', help_text='An alphanumeric value of the first name of this contact')
     lname = models.CharField(max_length=100, help_text='An alphanumeric value of the last name of this contact')
     phone = models.CharField(max_length=20, help_text='An alphanumeric value of the phone for this contact')
@@ -48,4 +44,3 @@ class Contact(models.Model):
 
     class Meta:
         db_table = "stn_contact"
-        verbose_name_plural = "contacts"
